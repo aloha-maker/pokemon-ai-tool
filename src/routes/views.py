818b4eddb354a2fd5c74982fa89f9ee586c20fb7
blue <daiki.basket15@gmail.com>
2025-10-15 00:31:14 +0900

    DB管理ボタン追加

--- src/routes/views.py
+++ src/routes/views.py
@@ -11,3 +11,8 @@ def index():
 def trained_pokemon_management():
     """育成済みポケモン管理ページを表示する。"""
     return render_template('trained_pokemon_management.html')
+
+@views_bp.route('/database')
+def database_viewer():
+    """データベース管理ページを表示する。"""
+    return render_template('database_viewer.html')
