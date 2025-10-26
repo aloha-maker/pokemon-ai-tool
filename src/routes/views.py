8ef564890479ac628da106571a638b9e3fd02c83
blue <daiki.basket15@gmail.com>
2025-10-26 23:18:39 +0900

    利用なしコードの削除

--- src/routes/views.py
+++ src/routes/views.py
@@ -6,8 +6,3 @@ views_bp = Blueprint('views', __name__)
 @views_bp.route('/')
 def index():
     return render_template('index.html')
-
-# @views_bp.route('/trained_pokemon_management')
-# def trained_pokemon_management():
-#     """育成済みポケモン管理ページを表示する。"""
-#     return render_template('trained_pokemon_management.html')
